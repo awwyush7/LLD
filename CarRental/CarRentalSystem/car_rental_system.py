b87113ec9dbb52427c212ac25a60ef3330ab9545
Ayush Pandey <pandeyayush22082002@gmail.com>
2026-03-07 17:53:18 +0530

    fixed bugs and errors

--- CarRental/CarRentalSystem/car_rental_system.py
+++ CarRental/CarRentalSystem/car_rental_system.py
@@ -1,17 +1,18 @@
 from Inventory.inventory import Inventory
 from ReservationManager.reservation_manager import ReservationManager
 from Payment.PaymentManager.payment_manager import PaymentManager
+from datetime import datetime  # or datetime, depending on your use case
 
 class CarRentalSystem:
-    def __init__(self, inventory : Inventory, reservation_manager : ReservationManager, payment_manager : PaymentManager):
+    def __init__(self, inventory: Inventory, reservation_manager: ReservationManager, payment_manager: PaymentManager):
         self.inventory = inventory
         self.reservation_manager = reservation_manager 
         self.payment_manager = payment_manager
     
-    def get_available_vehicles(self, from_date, to_date):
+    def get_available_vehicles(self, from_date: datetime, to_date: datetime):  # Added type annotations
         return self.reservation_manager.get_available_vehicles(from_date, to_date)
     
-    def make_reservation(self, user_id, vehicle_id, from_date, to_date):
+    def make_reservation(self, user_id: str, vehicle_id: str, from_date: datetime, to_date: datetime):  # Added type annotations
         try:
             reservation = self.reservation_manager.make_reservation(user_id, vehicle_id, from_date, to_date)
             bill = self.payment_manager.make_payment(reservation)
@@ -20,4 +21,3 @@ class CarRentalSystem:
         except Exception as e:
             print(e)
             return None
-    
--- CarRental/main.py
+++ CarRental/main.py
@@ -1,11 +1,16 @@
 from CarRentalSystem.car_rental_system import CarRentalSystem
 from Inventory.inventory import Inventory
+from Inventory.inventory import Inventory
 from ReservationManager.reservation_manager import ReservationManager
 from Payment.PaymentManager.payment_manager import PaymentManager
 from Vehicle.vehicle_conditions import VehicleCondidtion
 from Vehicle.vehicle_state import VehicleState
 from Vehicle.vehicle import Vehicle
+from datetime import datetime
 
+inventory = Inventory("Ayodhya")
+inventory.add_vehicle(Vehicle(12345, "car", "model1", 2016, VehicleCondidtion.HIGH, VehicleState.AVAILABLE, 90))
+inventory.add_vehicle(Vehicle(12346, "car", "model2", 2010, VehicleCondidtion.LOW, VehicleState.OUTOFSERVICE, 70))
 inventory = Inventory("Ayodhya")
 inventory.add_vehicle(Vehicle(12345, "car", "model1", 2016, VehicleCondidtion.HIGH, VehicleState.AVAILABLE, 90))
 inventory.add_vehicle(Vehicle(12346, "car", "model2", 2010, VehicleCondidtion.LOW, VehicleState.OUTOFSERVICE, 70))
@@ -14,11 +19,14 @@ payment_manager = PaymentManager("Stripe")
 
 
 car_rental_system = CarRentalSystem(inventory, reservation_manager, payment_manager)
-
-available = car_rental_system.get_available_vehicles("2024-06-01", "2024-06-10")
+start = datetime.strptime("2024-06-01", "%Y-%m-%d")
+end = datetime.strptime("2024-06-10", "%Y-%m-%d")
+available = car_rental_system.get_available_vehicles(start, end)
 print(available)
 bill = car_rental_system.make_reservation("user1", "vehicle1", "2024-06-01", "2024-06-10")
 
 print(bill)
+
+    
 
     
